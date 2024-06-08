852e44fa119b42a856268052179653bee1355817
indisofyar <38954340+indisofyar@users.noreply.github.com>
2024-06-08 15:12:34 +0200

    Connected to prod DB

--- luminal/luminal/settings/production.py
+++ luminal/luminal/settings/production.py
@@ -1,1 +1,10 @@
 from .base import *
+
+import dj_database_url
+
+DEBUG = False
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+DATABASES = {
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+}
