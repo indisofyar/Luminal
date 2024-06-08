7e382d336ae93860c1ff8d3c2f871acc2a819f58
indisofyar <38954340+indisofyar@users.noreply.github.com>
2024-06-08 16:56:40 +0200

    Added stuff

--- luminal/address/views.py
+++ luminal/address/views.py
@@ -9,15 +9,15 @@ from django.utils.dateparse import parse_datetime
 from address.models import Transaction
 
 
-
 # Create your views here.
 @api_view(['GET'])
 def hello_world(request):
     return Response('Hello world')
 
+
 @api_view(['GET'])
 def get_data(request, address):
-    url = "https://evm-sidechain.xrpl.org/api/v2/addresses/"+address+"/transactions"
+    url = "https://evm-sidechain.xrpl.org/api/v2/addresses/" + address + "/transactions"
     headers = {
         "Content-Type": "application/json"
     }
@@ -42,30 +42,71 @@ def get_data(request, address):
                 gas_used=int(item.get('gas_used', 0)),
                 priority_fee=int(item.get('priority_fee', 0)),
                 base_fee_per_gas=int(item.get('base_fee_per_gas', 0)),
-                total_gas_paid=(int(item.get('base_fee_per_gas', 0)) + int(item.get('priority_fee', 0))) * int(item.get('gas_used', 0)),
+                total_gas_paid=(int(item.get('base_fee_per_gas', 0)) + int(item.get('priority_fee', 0))) * int(
+                    item.get('gas_used', 0)),
                 error_status=item.get('result', ''),
                 revert_reason=item.get('revert_reason', '')
             )
             transactions.append(transaction)
 
         # Serializing data
-        transaction_data = [{
-            'transaction_hash': txn.transaction_hash,
-            'block_number': txn.block_number,
-            'address': txn.address,
-            'status': txn.status,
-            'from_address': txn.from_address,
-            'to_address': txn.to_address,
-            'method': txn.method,
-            'tx_type': txn.tx_type,
-            'timestamp': txn.timestamp,
-            'gas_used': txn.gas_used,
-            'priority_fee': txn.priority_fee,
-            'base_fee_per_gas': txn.base_fee_per_gas,
-            'total_gas_paid': txn.total_gas_paid,
-            'error_status': txn.error_status,
-            'revert_reason': txn.revert_reason
-        } for txn in transactions]
+        transaction_data = []
+        for txn in transactions:
+            transaction_data.append({
+                'transaction_hash': txn.transaction_hash,
+                'block_number': txn.block_number,
+                'address': txn.address,
+                'status': txn.status,
+                'from_address': txn.from_address,
+                'to_address': txn.to_address,
+                'method': txn.method,
+                'tx_type': txn.tx_type,
+                'timestamp': txn.timestamp,
+                'gas_used': txn.gas_used,
+                'priority_fee': txn.priority_fee,
+                'base_fee_per_gas': txn.base_fee_per_gas,
+                'total_gas_paid': txn.total_gas_paid,
+                'error_status': txn.error_status,
+                'revert_reason': txn.revert_reason
+            })
+
+            Transaction.objects.get_or_create(
+                transaction_hash=txn.transaction_hash,
+                defaults={
+                    'block_number': txn.block_number,
+                    'address': txn.address,
+                    'status': txn.status,
+                    'from_address': txn.from_address,
+                    'to_address': txn.to_address,
+                    'method': txn.method,
+                    'tx_type': txn.tx_type,
+                    'timestamp': txn.timestamp,
+                    'gas_used': txn.gas_used,
+                    'priority_fee': txn.priority_fee,
+                    'base_fee_per_gas': txn.base_fee_per_gas,
+                    'total_gas_paid': txn.total_gas_paid,
+                    'error_status': txn.error_status,
+                    'revert_reason': txn.revert_reason
+                }
+            )
+
+        # transaction_data = [{
+        #     'transaction_hash': txn.transaction_hash,
+        #     'block_number': txn.block_number,
+        #     'address': txn.address,
+        #     'status': txn.status,
+        #     'from_address': txn.from_address,
+        #     'to_address': txn.to_address,
+        #     'method': txn.method,
+        #     'tx_type': txn.tx_type,
+        #     'timestamp': txn.timestamp,
+        #     'gas_used': txn.gas_used,
+        #     'priority_fee': txn.priority_fee,
+        #     'base_fee_per_gas': txn.base_fee_per_gas,
+        #     'total_gas_paid': txn.total_gas_paid,
+        #     'error_status': txn.error_status,
+        #     'revert_reason': txn.revert_reason
+        # } for txn in transactions]
 
         return Response(transaction_data, status=status.HTTP_200_OK)
     except requests.exceptions.HTTPError as http_err:
