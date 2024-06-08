ce4f117e5f1455864631e1c6b0567934ddc119ab
indisofyar <38954340+indisofyar@users.noreply.github.com>
2024-06-08 16:43:38 +0200

    Created vue app

---
+++ frontend/CHANGELOG.md
@@ -0,0 +1,27 @@
+# CHANGELOG.md
+
+## [1.1.0] - 2023-12-07
+
+- Update Vite to 5
+- Update other dependencies
+
+## [1.0.4] - 2023-10-04
+
+- Update dependencies
+
+## [1.0.3] - 2023-05-06
+
+- Dependencies update
+
+## [1.0.2] - 2023-04-11
+
+- Dependency updates
+
+## [1.0.1] - 2023-02-13
+
+- Minor changes
+- Dependency updates
+
+## [1.0.0] - 2022-07-28
+
+First release
--- luminal/luminal/urls.py
+++ luminal/luminal/urls.py
@@ -22,11 +22,10 @@ from .views import index
 
 
 urlpatterns = [
-    path("", index),
+    path("", SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/address/', include('address.urls')),
     path("admin/", admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('documentation/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
